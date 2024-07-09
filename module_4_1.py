ae7e999889fe1997ba53903b15727513e5e21e4e
anna72nech <anna.v.nech@gmail.com>
2024-07-10 02:16:48 +0700

    Add files via upload
    Д/з к уроку Модули и пакеты

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as did_er
+from true_math import divide as did_inf
+
+result1 = did_er(69, 3)
+result2 = did_er(3, 0)
+result3 = did_inf(49, 7)
+result4 = did_inf(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
